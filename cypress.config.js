4187ba971dfd22cc7946435af1fb16f3571443d8
Ale <alelauhm@gmail.com>
2026-03-15 16:23:19 -0600

    Add mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-  
-  e2e: {
-    baseUrl: 'https://practicesoftwaretesting.com/',
-    defaultCommandTimeout: 10000,
-    retries: {
-      runMode: 1,
-      openMode: 0
+    allowCypressEnv: false,
+
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+        charts: true,
+        reportPageTitle: 'Practice Software Testing',
+        embeddedScreenshots: true,
+        inlineAssets: true,
+        saveAllAttempts: false,
     },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+    e2e: {
+        baseUrl: 'https://practicesoftwaretesting.com/',
+        defaultCommandTimeout: 10000,
+        retries: {
+            runMode: 1,
+            openMode: 0
+        },
+        setupNodeEvents(on, config) {
+            require('cypress-mochawesome-reporter/plugin')(on);
+        },
     },
-  },
 });
